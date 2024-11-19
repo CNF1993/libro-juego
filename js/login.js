632b9619a7286b46a0f998dfb6c2f934290c7bc9
CFN1993 <137438729+CFN1993@users.noreply.github.com>
2024-11-19 19:30:29 -0300

    change login.js

--- js/login.js
+++ js/login.js
@@ -3,7 +3,7 @@ document.getElementById("loginForm").addEventListener("submit", function (event)
     const username = document.getElementById("username").value.trim();
     const password = document.getElementById("password").value.trim();
   
-    if (username === "mel" && password === "123") {
+    if ((username === "mel" || username === "Mel" || username === "MEL") && password === "123") {
       window.location.href = `admin.html?user=${username}`;
     } else {
       document.getElementById("loginError").textContent = "Usuario o contraseña incorrectos.";
